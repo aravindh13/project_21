907c68cc6ca3731bf213da6a3954c8fce431b499
aravindh13 <70207026+aravindh13@users.noreply.github.com>
2020-09-01 21:28:53 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,49 @@
+var wall,thickness;
+var bullet,speed,weight;
+
 function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  createCanvas(1600,400);
+  
+  speed=random(223,321);
+  weight=random(38,52);
+  
+  thickness=random(22,53);
+  
+  bullet=createSprite(50,200,50,5)
+  bullet.velocityX=speed;
+  bullet.shapeColor=color(255);
+  
+  wall=createSprite(1200,288,thickness,height/2);
+  wall.shapeColour=color(238,230,230);
 }
 
 function draw() {
-  background(255,255,255);  
+  background(0); 
+  if(hascollided(bullet,wall))
+  {
+    bullet.velocityX=0;
+    var dmage=0.5=weight=speed=speed/(thickness=thickness=thickness);
+    
+    if(dmage>10)
+    {
+      wall.shapeColor=color(255,0,0);
+    }
+    
+    if(dmage<10)
+    {
+      wall.shapeColor=color(0,255,0);
+    }
+  }
   drawSprites();
+}
+
+function hasCollided(1bullet,1wall)
+{
+  bulletRightEdge=1bullet.x+1bullet.width;
+  wallLeftEdge=1wall.x;
+  if(bulletRightEdge=wallLeftEdge)
+  {
+    return true;
+  }
+  return false;
 }
